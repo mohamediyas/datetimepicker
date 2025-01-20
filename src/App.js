4e3690f3203739cdc7ad7d9e581eeadd3393a255
mohamediyas <50296178+mohamediyas@users.noreply.github.com>
2025-01-20 10:25:40 +0000

    date and time color added

--- src/App.js
+++ src/App.js
@@ -5,8 +5,57 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 import { useState } from "react";
 import dayjs, { Dayjs } from "dayjs";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import { Button } from "@mui/material";
+
+const theme = createTheme({
+  components: {
+    MuiPickersDay: {
+      styleOverrides: {
+        root: {
+          "&.Mui-selected": {
+            backgroundColor: "black !important",
+            color: "white !important",
+          },
+          "&.Mui-selected:hover": {
+            backgroundColor: "#333 !important",
+          },
+          "&:focus": {
+            backgroundColor: "black !important",
+            color: "white !important",
+          },
+        },
+      },
+    },
+    MuiPickersClockNumber: {
+      styleOverrides: {
+        root: {
+          "&.Mui-selected": {
+            backgroundColor: "black !important", // Black for selected time
+            color: "white !important", // White text for selected time
+          },
+          "&.Mui-selected:hover": {
+            backgroundColor: "#333 !important", // Slightly lighter on hover
+          },
+          "&:focus": {
+            backgroundColor: "black !important",
+          },
+        },
+      },
+    },
+    MuiPickersToolbarButton: {
+      styleOverrides: {
+        root: {
+          "&.Mui-selected": {
+            color: "black !important", // Black for selected AM/PM buttons
+            backgroundColor: "black !important",
+          },
+        },
+      },
+    },
+  },
+});
 
 export default function App() {
   const [selectedDate, setSelectedDate] = useState(null);
@@ -70,38 +119,40 @@ export default function App() {
 
   console.log("final", selectedDate, tempDate);
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DateTimePicker
-        // onClose={() => alert("closed")}
-        // disableOpenPicker
-        // closeOnSelect={false}
-        value={selectedDate} // Use tempDate until confirmed
-        onChange={handleDateChange}
-        // onAccept={handleAccept} // Update only on accept (OK button click)
-        open={open}
-        onOpen={() => setOpen(true)}
-        onClose={handleClose}
-        slots={{
-          actionBar: CustomActionBar, // Replace the default ActionBar
-        }}
-        slotProps={{
-          layout: {
-            sx: {
-              display: "flex",
-              flexDirection: "column",
-              gap: "12px",
-              padding: "12px",
-              alignItems: "flex-end",
+    <ThemeProvider theme={theme}>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DateTimePicker
+          // onClose={() => alert("closed")}
+          // disableOpenPicker
+          // closeOnSelect={false}
+          value={selectedDate} // Use tempDate until confirmed
+          onChange={handleDateChange}
+          // onAccept={handleAccept} // Update only on accept (OK button click)
+          open={open}
+          onOpen={() => setOpen(true)}
+          onClose={handleClose}
+          slots={{
+            actionBar: CustomActionBar, // Replace the default ActionBar
+          }}
+          slotProps={{
+            layout: {
+              sx: {
+                display: "flex",
+                flexDirection: "column",
+                gap: "12px",
+                padding: "12px",
+                alignItems: "flex-end",
+              },
             },
-          },
-        }}
-        // onClose={() => setOpen(false)}
-        // autoFocus
-        // value={value}
-        // onChange={(valuesf) => setValue(valuesf)}
-        // onAccept={(valuesf) => setValue(valuesf)}
-        views={["year", "day", "hours", "minutes", "seconds"]}
-      />
-    </LocalizationProvider>
+          }}
+          // onClose={() => setOpen(false)}
+          // autoFocus
+          // value={value}
+          // onChange={(valuesf) => setValue(valuesf)}
+          // onAccept={(valuesf) => setValue(valuesf)}
+          views={["year", "day", "hours", "minutes", "seconds"]}
+        />
+      </LocalizationProvider>
+    </ThemeProvider>
   );
 }
